eec0fe3141c08aac66c1c6676adc6e96f49149e2
almahmudsarker <almahmuds427@gmail.com>
2023-11-01 22:10:13 +0600

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import FileGrid from "./components/FileGrid";
-
 function App() {
   return (
     <>
       <h1 className="text-7xl font-bold text-center">olloy</h1>
-      <FileGrid />
     </>
   );
 }
